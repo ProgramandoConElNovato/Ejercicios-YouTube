a356a8cec05561b340e171327bf74c97f62205b8
Programando con el novato <programandoconelnovato@gmail.com>
2024-07-17 14:15:44 +0200

    Add files via upload
    Primeros códigos del canal correspondiente a los tres primeros vídeos. Dos de teoría y uno de ejercicio.

---
+++ Java/Teorías/1 - VARIABLES y tipos PRIMITIVOS/PrimeraClase.java
@@ -0,0 +1,43 @@
+package primerPaquete;
+
+public class PrimeraClase {
+	
+	public static void main(String[] args) {
+		
+		int primeraVariable;
+		
+		//Número enteros.
+		
+		byte numeroByte;
+		
+		numeroByte = 50;
+		
+		short numeroShort = -12563;
+		
+		int numeroInt = 978431567;
+		
+		long numeroLong = 95238129562L;
+		
+		//Número decimales.
+		
+		float numeroFloat = 4.782F;
+		
+		double numeroDouble = 0.852217263;
+		
+		// Carácteres.
+		
+		char caracter = '$';
+		
+		//Booleano o lógico.
+		
+		boolean booleano = false;
+		
+		//Tipo objeto.
+		
+		String frase = "Gracias por ver este vídeo";
+		
+		System.out.print(frase);
+		
+	}
+
+}
---
+++ Java/Teorías/2 - OPERADORES/Operadores.java
@@ -0,0 +1,21 @@
+package ejerciciosVariables;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		
+		double num1=5;
+		
+		double num2=5;
+		
+		double num3=10;
+		
+		/*double resultado;
+
+		resultado = num1 % num2;*/
+		
+		System.out.println(num1 == num2 || num1 == num3);
+
+	}
+
+}
