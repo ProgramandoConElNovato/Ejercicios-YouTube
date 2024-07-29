bbfec5b4d0c23eede13d294272ae5b244d469d72
Programando con el novato <programandoconelnovato@gmail.com>
2024-07-29 14:11:46 +0200

    Add files via upload
    Código correspondiente al vídeo de teoría 4 - Arrays del canal.

---
+++ Java/Teorías/4 - ARRAYS/Arrays.java
@@ -0,0 +1,21 @@
+package teoria;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		
+		int arrayInt[][] = new int[2][3];
+		
+		arrayInt[0][0] = 100;
+		arrayInt[0][1] = 200;
+		arrayInt[0][2] = 300;
+		
+		arrayInt[1][0] = 700;
+		arrayInt[1][1] = 800;
+		arrayInt[1][2] = 900;
+		
+		System.out.println(arrayInt[1][0]);
+
+	}
+
+}
